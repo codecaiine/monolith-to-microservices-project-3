b057c31574b14ea6f83eef72e6128eb3146be275
codecaiine <akabrouyannickn@gmail.com>
2022-11-05 13:19:22 +0000

    Add

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=nybadb
+export POSTGRES_PASSWORD=bZaTG#Ing2TbnK
+export POSTGRES_HOST=cdr.cper4vkom0vo.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:nyba-0709981789-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
